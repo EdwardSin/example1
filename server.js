c51eb9afa8a8cd40fe5f297a72a2ae14016269f4
Zhou Gong <edwardsin99@hotmail.com>
2018-06-19 20:57:17 +0800

    Server Changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.use(express.static(__dirname + "/dist"));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src' ,'index.html'));
 })
 app.listen(process.env.PORT || 8080);
 console.log("Console listening!");
