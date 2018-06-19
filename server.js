ea2a17f874a80917d5eee300a1e6cdcee77af344
Zhou Gong <edwardsin99@hotmail.com>
2018-06-19 17:16:14 +0800

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(__dirname + "/dist/example"));
+console.log(__dirname);
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/example/index.html'));
