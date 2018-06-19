1d1fd3fff72d9bc2c693c89280d3327a204cd7a2
Zhou Gong <edwardsin99@hotmail.com>
2018-06-19 18:30:09 +0800

    make it better

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "dist")));
-console.log(__dirname);
+
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 })
 
 console.log("Console listening!");
